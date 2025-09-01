7787edc42165bf9c418575f0b6c97743c5d2c38f
Wuttipat Pipopsukawadee <t.wuttipat.pipopsukawadee@TECHXMACI25030A.local>
2025-09-01 09:59:53 +0700

    try to push git repo

--- jenkins-library/vars/stageMoveTerraformModule.groovy
+++ jenkins-library/vars/stageMoveTerraformModule.groovy
@@ -5,94 +5,98 @@ def call(Map args) {
         sh "mkdir -p template"
         sh "mkdir -p project"
         container('git') {
-        try {
-            dir('template') {
-                //container('git') {
-                    git url: args.TemplateRepo, branch: 'main'
-                //}
-                echo "dir template"
-                sh "ls -al"
+            try {
+                dir('template') {
+                    //container('git') {
+                        git url: args.TemplateRepo, branch: 'main'
+                    //}
+                    echo "dir template"
+                    sh "ls -al"
+                }
+            } catch (err) {
+                error "Git clone failed: ${err.message}"
             }
-        } catch (err) {
-            error "Git clone failed: ${err.message}"
-        }
 
-        try {
+            try {
+                dir('project') {
+                    //container('git') {
+                        git url: args.GitProjectMetadataRepo, branch: args.GitBranch
+                    //}
+                    echo "dir project"
+                    sh "ls -al"
+                }
+            } catch (err) {
+                error "Git clone failed: ${err.message}"
+            }
+
             dir('project') {
-                //container('git') {
-                    git url: args.GitProjectMetadataRepo, branch: args.GitBranch
-                //}
-                echo "dir project"
-                sh "ls -al"
+                sh """
+                    for folder in terraform-configuration terraform-output terraform-module
+                    do
+                        if [ ! -d "\$folder" ]; then
+                            echo "Folder \$folder not found, creating..."
+                            mkdir -p "\$folder"
+                        else
+                            echo "Folder \$folder already exists"
+                        fi
+                    done
+                """
             }
-        } catch (err) {
-            error "Git clone failed: ${err.message}"
-        }
-
-        dir('project') {
+            
+            def subdir = "${args.TerraformModule}-${args.ServiceName}"
+            dir('project') {
+                sh """
+                    for folder in terraform-configuration terraform-output terraform-module
+                    do
+                    if [ ! -d "\$folder/${subdir}" ]; then
+                        echo "Subfolder \$folder/${subdir} not found, creating"
+                        mkdir -p "\$folder/${subdir}"
+                    else
+                        echo "Subfolder \$folder/${subdir} already exists"
+                    fi
+                    done
+                """
+            }
             sh """
-                for folder in terraform-configuration terraform-output terraform-module
-                do
-                  if [ ! -d "$folder" ]; then
-                    echo "Folder $folder not found, creating..."
-                    mkdir -p "$folder"
-                  else
-                    echo "Folder $folder already exists"
-                  fi
-                done
-            """
-        }
-        def subdir = "${args.TerraformModule}-${args.ServiceName}"
-        dir('project') {
-            sh """
-                for folder in terraform-configuration terraform-output terraform-module
-                do
-                  if [ ! -d "\$folder/${subdir}" ]; then
-                    echo "Subfolder \$folder/${subdir} not found, creating"
-                    mkdir -p "\$folder/${subdir}"
-                  else
-                    echo "Subfolder \$folder/${subdir} already exists"
-                  fi
-                done
-            """
-        }
-        sh """
-            if [ -f template/${args.TerraformModule}/config.json ]; then
-                cp template/${args.TerraformModule}/config.json project/terraform-configuration/\${subdir}
-                echo "Copied config.json"
-            fi
-
-            if [ -f template/${args.TerraformModule}/output.tf ]; then
-                cp template/${args.TerraformModule}/output.tf project/terraform-output/\${subdir}
-                echo "Copied output.tf"
-            fi
-
-            for f in backend.tf main.tf Makefile provider.tf variables.tf
-            do
-                if [ -f template/${args.TerraformModule}/\$f ]; then
-                    cp template/${args.TerraformModule}/\$f project/terraform-module/\${subdir}
-                    echo "Copied \$f"
+                if [ -f template/${args.TerraformModule}/config.json ]; then
+                    cp template/${args.TerraformModule}/config.json project/terraform-configuration/\${subdir}
+                    echo "Copied config.json"
                 fi
-            done
-            ls -alR project
-        """
 
-        
+                if [ -f template/${args.TerraformModule}/output.tf ]; then
+                    cp template/${args.TerraformModule}/output.tf project/terraform-output/\${subdir}
+                    echo "Copied output.tf"
+                fi
+
+                for f in backend.tf main.tf Makefile provider.tf variables.tf
+                do
+                    if [ -f template/${args.TerraformModule}/\$f ]; then
+                        cp template/${args.TerraformModule}/\$f project/terraform-module/\${subdir}
+                        echo "Copied \$f"
+                    fi
+                done
+                ls -alR project
+            """
+
+            
             dir('project') {
                 withCredentials([string(credentialsId: 'initialpipeline-gitcreaterepo', variable: 'GITHUB_TOKEN')]) {
                     sh """
-                        ls -al
-                        git config --global --add safe.directory /home/jenkins/agent/workspace/infra-automation/request/project
+                        set -e
+
+                        git config user.name  "Jenkins"
+                        git config user.email "jenkins@example.com"
+
+                        git checkout -B ${args.GitBranch}
+
+                        git remote set-url origin "https://\${GITHUB_TOKEN}@github.com/wuttipat6509650716/project-test-infra-automation.git"
+
                         git status
-                        git config user.email "jenkins"
-                        git config user.name "Jenkins User"
-                        git add .
-                        if ! git diff --cached --quiet; then
-                            git commit -m "Update terraform files for ${subdir}"
-                            git push https://x-access-token:${GITHUB_TOKEN}@github.com/wuttipat6509650716/project-test-infra-automation.git HEAD:${args.GitBranch}
-                        else
-                            echo "No changes to commit"
-                        fi
+                        git add -A
+
+                        git diff --cached --quiet || git commit -m "chore: update terraform module ${args.TerraformModule}-${args.ServiceName}"
+
+                        git push -u origin ${args.GitBranch}
                     """
                 }
             }
