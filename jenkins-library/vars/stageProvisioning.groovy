b7d99662f8c851041ef1db40374ec21b8026eef4
Wuttipat Pipopsukawadee <t.wuttipat.pipopsukawadee@ip-192-168-240-117.ap-southeast-1.compute.internal>
2025-09-08 17:00:08 +0700

    format provission

--- jenkins-library/vars/stageProvisioning.groovy
+++ jenkins-library/vars/stageProvisioning.groovy
@@ -1,51 +1,50 @@
 // vars/stageProvisioning.groovy
-
 def call(Map args) {
-  stage("Provisioning") {
+  stage('Provisioning') {
     container('terraform') {
-      sh "apk add --no-cache make bash"
+      sh 'apk add --no-cache make bash'
 
-      withCredentials([azureServicePrincipal(
-        credentialsId: 'azure-service-principal',
-        subscriptionIdVariable: 'ARM_SUBSCRIPTION_ID',
-        clientIdVariable:       'ARM_CLIENT_ID',
-        clientSecretVariable:   'ARM_CLIENT_SECRET',
-        tenantIdVariable:       'ARM_TENANT_ID'
-        )]) {
-          sh """
-            cd project/terraform-module/${args.TerraformName}
-            make init
-            make plan CONFIG_FILE=../../terraform-configuration/${args.TerraformName}/config.json
-          """
-          if(args.SKIP_APPROVAL == false){
-            timeout(time: 15, unit: 'MINUTES') {
-                input message: "Approve Terraform Apply?", ok: "Apply Now"
+      withCredentials([
+        azureServicePrincipal(
+          credentialsId:            'azure-service-principal',
+          subscriptionIdVariable:   'ARM_SUBSCRIPTION_ID',
+          clientIdVariable:         'ARM_CLIENT_ID',
+          clientSecretVariable:     'ARM_CLIENT_SECRET',
+          tenantIdVariable:         'ARM_TENANT_ID'
+        )
+      ]) {
+        sh """
+          set -e
+          cd project/terraform-module/${args.TerraformName}
+          make init
+          make plan CONFIG_FILE=../../terraform-configuration/${args.TerraformName}/config.json
+        """
+
+        if (!args.SKIP_APPROVAL) {
+          timeout(time: 15, unit: 'MINUTES') {
+            def userChoice = input(
+              id: 'TerraformApproval',
+              message: 'Approve Terraform Apply?',
+              parameters: [
+                choice(
+                  name:        'ACTION',
+                  choices:     ['Apply', 'Abort'],
+                  description: 'Select action'
+                )
+              ]
+            )
+            if (userChoice == 'Abort') {
+              error('User aborted the Terraform Apply.')
             }
           }
-
-          sh """
-            cd project/terraform-module/${args.TerraformName}
-            make apply AUTO_APPROVE=true
-          """
         }
+
+        sh """
+          set -e
+          cd project/terraform-module/${args.TerraformName}
+          make apply AUTO_APPROVE=true
+        """
       }
     }
+  }
 }
-
-
-// 
-// withAzureCredentials(bindings: [azureServicePrincipal('azure-service-principal')]) {
-//   container('terraform') {
-//     sh '''
-//       set -e
-//       export ARM_CLIENT_ID="$AZURE_CLIENT_ID"
-//       export ARM_CLIENT_SECRET="$AZURE_CLIENT_SECRET"
-//       export ARM_TENANT_ID="$AZURE_TENANT_ID"
-//       export ARM_SUBSCRIPTION_ID="$AZURE_SUBSCRIPTION_ID"
-
-//       terraform -version
-//       terraform init
-//       terraform apply -auto-approve
-//     '''
-//   }
-// }
